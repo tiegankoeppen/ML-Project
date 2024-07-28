0d19102bfee0ae3c324c119cf7c882e7ac00f1da
Kobe Yang <kobeyang160@gmail.com>
2024-07-28 16:48:00 -0500

    Set up stricter ESlint rules for frontend and made fixes to make sure no errors were thrown

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -3,12 +3,27 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked', 
+    'plugin:@typescript-eslint/stylistic-type-checked', 
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended', 
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  }, 
   rules: {
     'react-refresh/only-export-components': [
       'warn',
